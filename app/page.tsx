02b671d8e1f60068f65d60ea8895aa94240ab07b
clubsodagod <maliekdavis@outlook.com>
2024-09-04 09:17:35 -0400

    home page snapto removed

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
           const id = entry.target.id;
           setCurrentSection(id); // Update currentSection to the current ref's id
           controls.start(id); // Trigger the animation for the current section
-          snapToTop(entry.target);
+          // snapToTop(entry.target);
         console.log(entry.target.id);
         }
         
