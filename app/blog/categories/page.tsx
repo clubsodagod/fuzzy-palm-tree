5b2b697324751d66fe2f7cb72f7090d9ca01d323
clubsodagod <maliekdavis@outlook.com>
2024-11-12 19:56:37 -0500

    finally live - 19

--- app/blog/categories/page.tsx
+++ app/blog/categories/page.tsx
@@ -7,15 +7,26 @@ import { MotionH3 } from '@/app/_components/framer/MotionH3'
 import { MotionH5 } from '@/app/_components/framer/MotionH5'
 import { MotionH6 } from '@/app/_components/framer/MotionH6'
 import { MotionP } from '@/app/_components/framer/MotionP'
-import { ICategoryPopulated } from '@/library/db/models/category'
+import CategoryModel, { ICategoryPopulated } from '@/library/db/models/category'
 import { getAllCategoriesClient } from '@/utility/blog-section/blog-page-functions'
 import React from 'react'
 import MotionCategoryCard from '../_components/MotionCategoryCard'
 import styles from '../styles.module.css'
+import { connect } from 'http2'
+import { connectToMongoDB } from '@/library/db/db'
+import { SubcategoryModel } from '@/library/db/models'
 
 export default async function CategoriesPage() {
 
-    const categories = await getAllCategoriesClient();
+    await connectToMongoDB()
+    await SubcategoryModel.find()
+    const categoriesResponse = await CategoryModel.find().populate('subcategories') as unknown as ICategoryPopulated[];
+
+    let categories
+
+    if (categories) {
+        categories = categoriesResponse
+    }
 
     // handle url string function
     function url(slug: string) {
@@ -42,7 +53,7 @@ export default async function CategoriesPage() {
 
                 {/* category cards */}
                 {
-                    categories.length > 0 &&
+                    categories && categories.length > 0 &&
                     <MotionDiv
                     className={`${styles.categoriesCardCtn}`}
                     >
